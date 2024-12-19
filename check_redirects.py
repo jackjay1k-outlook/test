03f34495e7f590a0761460133be17a5e216d683d
jackjay1k-outlook <jackjay1k@outlook.com>
2024-12-19 22:39:07 +0800

    Update check_redirects.py

--- check_redirects.py
+++ check_redirects.py
@@ -1,5 +1,6 @@
 import requests
 from urllib.parse import urlsplit
+import time
 
 # 在这里添加你收藏的链接
 urls = [
@@ -11,7 +12,11 @@ urls = [
 
 headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
+    "Referer": "https://www.google.com",
+    "DNT": "1",
+    "Upgrade-Insecure-Requests": "1",
+    "Accept-Language": "zh-CN,en;q=0.9"
 }
 
 def get_base_url(url):
@@ -19,9 +24,10 @@ def get_base_url(url):
 
 def check_urls(urls):
     results = []
+    session = requests.Session()
     for url in urls:
         try:
-            response = requests.get(url, allow_redirects=True, headers=headers)
+            response = session.get(url, allow_redirects=True, headers=headers)
             final_base_url = get_base_url(response.url).rstrip('/')
             original_base_url = get_base_url(url).rstrip('/')
             if response.history and final_base_url != original_base_url:
@@ -37,6 +43,7 @@ def check_urls(urls):
             #     results.append(f"{url} (· 有效 ·)")
         except requests.RequestException as e:
             results.append(f"{url} (· 检查时出错: {e} ·)")
+        time.sleep(2)  # 每次请求之间添加2秒的延迟
     return results
 
 if __name__ == "__main__":
